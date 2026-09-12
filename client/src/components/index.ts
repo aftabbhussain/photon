401d30c22548b93c29238d86a939eaf77d627dc5
aftabbhussain <88221488+aftabbhussain@users.noreply.github.com>
2026-09-13 00:39:51 +0530

    feat(components) : wrap up the components dev by adding the indexing

---
+++ client/src/components/index.ts
@@ -0,0 +1,8 @@
+export * from "./common/ToastContainer";
+export * from "./layout/Header";
+export * from "./layout/Footer";
+export * from "./transfer";
+
+export * from "./sections";
+export * from "./modals";
+
